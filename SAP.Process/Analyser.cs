3b970c23f188b793e5c54b95ba03c2e1f25dd87e
Daniel Worrall <dworrall@healthcomm.co.uk>
2017-03-09 15:16:51 +0000

    Added copyright notice and statement of copying permission to each relevant source file. Removed unnecessary using statements.

--- SAP.Process/Analyser.cs
+++ SAP.Process/Analyser.cs
@@ -1,9 +1,27 @@
-﻿using System;
+﻿/*
+    Copyright 2016 Healthcare Communications UK Ltd
+ 
+    This file is part of HCSentimentAnalysisProcessor.
+
+    HCSentimentAnalysisProcessor is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    HCSentimentAnalysisProcessor is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with HCSentimentAnalysisProcessor.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using java.util;
 using edu.stanford.nlp.ling;
 using edu.stanford.nlp.neural.rnn;
@@ -14,7 +32,6 @@ using edu.stanford.nlp.trees;
 using SAP.Dtos;
 using SAP.Interfaces;
 using SAP.Interfaces.Dtos;
-using System.Configuration;
 using System.Diagnostics;
 
 
