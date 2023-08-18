55aa7ace7ca089eb2970980a9360383707fade95
Stew Elliott <sellio@cisco.com>
2023-08-18 16:46:20 +0100

    Removing local machine path

--- SAP.DataModel/SentimentModel.Designer.cs
+++ SAP.DataModel/SentimentModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\sellio\source\repos\SAProcessing3\SAP.DataModel\SentimentModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\GIT Repos\envoy-messenger\SentimentAnalysis - Open Source\ESA.DataModel\SentimentModel.edmx'.
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
