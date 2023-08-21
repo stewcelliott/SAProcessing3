68638e38979f694e95698cf437b42c1f7475c9c4
Stew Elliott <sellio@cisco.com>
2023-08-21 11:16:15 +0100

    Removing commented out code

--- SAP.Interfaces/IEngine.cs
+++ SAP.Interfaces/IEngine.cs
@@ -30,12 +30,7 @@ namespace SAP.Interfaces
         void FinishBatch(ISentimentBatchDto sentimentBatch);
         List<ISentimentQueueDto> GetSentimentQueueForProcessing(int batchSize, bool retryFailed = false);
         void SaveSentimentQueueProcessingOutcome(ISentimentQueueDto sentimentQueue);
-        //TODO: Remove commented items if superfluous
-        //List<ISentimentDto> GetSentiments();
-        //ISentimentDto GetSentimentById(int id);
+
         void SaveSentiment(ISentimentDto sentiment);
-        //List<ISentimentSentenceDto> GetSentimentSentences(ISearchFilterDto searchFilter);
-        //ISentimentSentenceDto GetSentimentSentence(int id);
-        //bool FlagSentimentHiddenById(int id);
     }
 }
